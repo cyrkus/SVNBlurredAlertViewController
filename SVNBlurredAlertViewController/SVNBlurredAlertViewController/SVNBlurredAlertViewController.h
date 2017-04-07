e9f7bb94e386cce35aaf56008adf619acc72e522
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-07 16:10:51 -0700

    add xcodeproj and make public

---
+++ SVNBlurredAlertViewController/SVNBlurredAlertViewController/SVNBlurredAlertViewController.h
@@ -0,0 +1,19 @@
+//
+//  SVNBlurredAlertViewController.h
+//  SVNBlurredAlertViewController
+//
+//  Created by Aaron Dean Bikis on 4/7/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNBlurredAlertViewController.
+FOUNDATION_EXPORT double SVNBlurredAlertViewControllerVersionNumber;
+
+//! Project version string for SVNBlurredAlertViewController.
+FOUNDATION_EXPORT const unsigned char SVNBlurredAlertViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNBlurredAlertViewController/PublicHeader.h>
+
+
